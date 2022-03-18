1ef3d7c5351231b9b1fac2591967d79da604259f
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-18 10:48:09 +0900

    기본 UI세팅

--- app/src/main/java/com/zeroheat/lolandroll/SplashActivity.kt
+++ app/src/main/java/com/zeroheat/lolandroll/SplashActivity.kt
@@ -1,12 +1,11 @@
 package com.zeroheat.lolandroll
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_splash)
     }
 }
---
+++ app/src/main/java/com/zeroheat/lolandroll/api/APIList.kt
@@ -0,0 +1,4 @@
+package com.zeroheat.lolandroll.api
+
+interface APIList {
+}
