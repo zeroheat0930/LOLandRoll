1b092ec10190f569da2762a4ea8b50d13153f486
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-18 11:11:06 +0900

    아이콘다운..

--- app/src/main/java/com/zeroheat/lolandroll/SplashActivity.kt
+++ app/src/main/java/com/zeroheat/lolandroll/SplashActivity.kt
@@ -3,9 +3,19 @@ package com.zeroheat.lolandroll
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
     }
 }
