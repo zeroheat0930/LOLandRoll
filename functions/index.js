fd9410bff56a3a6ae13ccc4c5b6d5ae262fc6bd5
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-21 11:47:05 +0900

    project에 funtions등록

--- functions/index.js
+++ functions/index.js
@@ -3,10 +3,8 @@ const cors = require('cors')({origin: true});
 const request = require('request');
 exports.apicall = functions.https.onRequest((req, response) => {
   cors(req, response, () => {
-     if(req.get('origin') === 'https://lolandroll-543b4.firebaseapp.com' || req.get('origin') === 'http://localhost:5000'){
-          request(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/hideonbush?api_key=RGAPI-674f74e7-1c47-41ca-bdd5-6bad2a4ea7d9`, function (error, res, body) {
-            response.send(res);
-          });
-        }
-      })
+    request(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/hideonbush?api_key=RGAPI-674f74e7-1c47-41ca-bdd5-6bad2a4ea7d9`, function (error, res, body) {
+      response.send(res);
     });
+  })
+});
