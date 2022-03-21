de86aabab253c5d066efdf9c19a60ffe6c2fdfda
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-21 10:24:53 +0900

    푸시알림 체험중  - FCM 라이브러리 설치 (Tools - Firebase)  - 디바이스 토큰 불러와서 로그로 확인

--- app/src/main/java/com/zeroheat/lolandroll/SplashActivity.kt
+++ app/src/main/java/com/zeroheat/lolandroll/SplashActivity.kt
@@ -2,6 +2,8 @@ package com.zeroheat.lolandroll
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessaging
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,11 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+    getFCMDeviceToken()
+    }
 
+    fun getFCMDeviceToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener {
+            Log.d("토큰값", it.result!!) }
     }
 }
