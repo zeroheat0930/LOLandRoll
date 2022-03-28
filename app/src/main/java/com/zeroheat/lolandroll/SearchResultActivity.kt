c0a331ec1ca02429471cf683f59214f59ae7d259
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-28 17:20:38 +0900

    깨닳았다..

--- app/src/main/java/com/zeroheat/lolandroll/SearchResultActivity.kt
+++ app/src/main/java/com/zeroheat/lolandroll/SearchResultActivity.kt
@@ -15,30 +15,27 @@ import com.zeroheat.lolandroll.databinding.ActivityParcticBinding
 import com.zeroheat.lolandroll.datas.LeagueResponse
 import com.zeroheat.lolandroll.datas.MatchDetailData
 import com.zeroheat.lolandroll.datas.SummonerResponse
-import com.zeroheat.lolandroll.recyclerview.DataItem
 import com.zeroheat.lolandroll.recyclerview.FirstData
-import com.zeroheat.lolandroll.recyclerview.SecondData
-import com.zeroheat.lolandroll.recyclerview.ThirdData
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class SearchResultActivity : BaseActivity() {
-    lateinit var binding : ActivityParcticBinding
+    lateinit var binding: ActivityParcticBinding
 
     lateinit var mAdapter: SearchUserrRecyclerAdapter
 
 
     val mFirstList = ArrayList<FirstData>()
-    val mSecondList = ArrayList<SecondData>()
-    val mThirdList = ArrayList<ThirdData>()
+    val mThisSeasonRankList = ArrayList<LeagueResponse>()
+    val mMatchDetailList = ArrayList<MatchDetailData>()
 
 
-    lateinit var summonerInfo : SummonerResponse
+    lateinit var summonerInfo: SummonerResponse
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= DataBindingUtil.setContentView(this, R.layout.activity_parctic)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_parctic)
         summonerInfo = intent.getSerializableExtra("summoner") as SummonerResponse
         setupEvents()
         setValues()
@@ -47,180 +44,52 @@ class SearchResultActivity : BaseActivity() {
 
     override fun setupEvents() {
 
-        mAdapter = SearchUserrRecyclerAdapter(mContext, mFirstList, mSecondList, mThirdList)
+        mAdapter = SearchUserrRecyclerAdapter(mContext, summonerInfo.puuid, mFirstList, mThisSeasonRankList, mMatchDetailList)
         binding.threeRecycle.adapter = mAdapter
         binding.threeRecycle.layoutManager = LinearLayoutManager(mContext)
 
 
-
-//        json파싱만 사용해서 데이터 테이블에 넣어보자
-        apiList.getLeague(
-            summonerInfo.id,
-            "RGAPI-25102a0e-d805-443c-8449-e51b6a10f4c3"
-        ).enqueue(object : Callback<List<LeagueResponse>>{
-            override fun onResponse(
-                call: Call<List<LeagueResponse>>,
-                response: Response<List<LeagueResponse>>
-            ) {
-                val list = response.body()!!
-            }
-
-            override fun onFailure(call: Call<List<LeagueResponse>>, t: Throwable) {
-
-            }
-        })
-
-
-
-
-
 //                  성공하면 소환사 랭크 리그 정보를 넣어줌.
-        apiList.getLeague(
-            summonerInfo.id,
-            "RGAPI-25102a0e-d805-443c-8449-e51b6a10f4c3"
-        ).enqueue(object: Callback<List<LeagueResponse>> {
-            override fun onResponse(
-                call: Call<List<LeagueResponse>>,
-                response: Response<List<LeagueResponse>>
-            ) {
-                val list = response.body()!!
-//                                파이어베이스 데이터 넣기
-                if(list.size != 0 ) {
-                    realtimeDB.getReference("League").child(summonerInfo.name)
-                        .setValue(list[0])
-
-                    mMatchIdList.add(list[0].leagueId)
-                }
-
-//                                데이터베이스에 puuid를 꺼내옴.
-                realtimeDB.getReference("Summoner").addValueEventListener(object :
-                    ValueEventListener {
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                        val value = snapshot.children.last().child(summonerInfo.puuid).key.toString()
-
-                        Log.d("value", value)
-//                                       puuid넣은 최근 30게임 전적이름 가져와서 데이터베이스 넣어줌.
-                        apiList3.getMatch(
-                            value,
-                            "20",
-                            "RGAPI-25102a0e-d805-443c-8449-e51b6a10f4c3")
-                            .enqueue(object : Callback<List<String>> {
-                                override fun onResponse(
-                                    call: Call<List<String>>,
-                                    response: Response<List<String>>
-                                ) {
-                                    val a = response.body()!!
-                                    Log.d("로그a", a.toString())
-//                                    소환사 매치 목록 20개를 match데이터 베이스에 소환사 이름별대로 저장을함.
-                                    realtimeDB.getReference("Match").child(summonerInfo.name).setValue(a)
-//                                      매치에 등록된 데이터를 꺼내오는 코드 시작
-                                    realtimeDB.getReference("Match").addValueEventListener(object : ValueEventListener{
-                                        override fun onDataChange(snapshot: DataSnapshot) {
-//                                              스냅샷이 소환사이름데이터일때 데이터를 mMatchIdList에 넣어줌.
-                                            for (matchItem in snapshot.child(summonerInfo.name).children) {
-
-//                                                Log.d("매치id목록", matchItem.value.toString())
-
-                                                apiList3.getMatchDetail(
-                                                    matchItem.value.toString(),
-                                                    "RGAPI-25102a0e-d805-443c-8449-e51b6a10f4c3").enqueue(object :
-                                                    Callback<MatchDetailData> {
-                                                    override fun onResponse(
-                                                        call: Call<MatchDetailData>,
-                                                        response: Response<MatchDetailData>
-                                                    ) {
-                                                        val b = response.body()!!
-
-//                                                        Log.d("b", b.toString())
-                                                        realtimeDB.getReference("MatchDetail").child(summonerInfo.name).setValue(b)
-
-                                                        realtimeDB.getReference("MatchDetail").addValueEventListener(object : ValueEventListener{
-                                                            override fun onDataChange(snapshot: DataSnapshot) {
-                                                                val abab = snapshot.child(summonerInfo.name).child("info").child("participants")
-
-                                                                for (item in abab.children) {
-                                                                    val puuId = item.child("puuid").value.toString()
-                                                                    if (puuId == summonerInfo.puuid) {
-
-                                                                        Log.d("abab내부item", item.toString())
-                                                                        Log.d("puuid값", puuId)
-                                                                    }
-                                                                }
-
-
-
-
-
-                                                            }
-
-                                                            override fun onCancelled(error: DatabaseError) {
-
-                                                            }
-                                                        })
-
-                                                    }
-
-                                                    override fun onFailure(
-                                                        call: Call<MatchDetailData>,
-                                                        t: Throwable
-                                                    ) {
-
-                                                    }
-                                                })
-
-
-//                                                mMatchIdList.clear()
-                                                mMatchIdList.add(matchItem.value.toString())
-                                                mAdapter.notifyDataSetChanged()
-
-                                            }
-
-
-
-
-
-
-
-
-                                        }
-
-                                        override fun onCancelled(error: DatabaseError) {
-
-                                        }
-                                    })
-
-                                }
-
-                                override fun onFailure(
-                                    call: Call<List<String>>,
-                                    t: Throwable
-                                ) {
-
-                                }
-
-                            })
-                    }
-
-                    override fun onCancelled(error: DatabaseError) {
-
-                    }
-                })
-
-
-            }
-
-            override fun onFailure(call: Call<List<LeagueResponse>>, t: Throwable) {
-
-            }
-        })
-
-
-
-
-//        val recyclerView = findViewById<RecyclerView>(R.id.threeRecycle)
-//        val manager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-//        recyclerView.layoutManager = manager
-//        recyclerView.adapter = SearchUserrRecyclerAdapter(dataList)
+//        apiList.getLeague(
+//            summonerInfo.id,
+//            "RGAPI-25102a0e-d805-443c-8449-e51b6a10f4c3"
+//        ).enqueue(object: Callback<List<LeagueResponse>> {
+//            override fun onResponse(
+//                call: Call<List<LeagueResponse>>,
+//                response: Response<List<LeagueResponse>>
+//            ) {
+//                val list = response.body()!!
+////                                파이어베이스 데이터 넣기
+//                if(list.size != 0 ) {
+//                    realtimeDB.getReference("League").child(summonerInfo.name)
+//                        .setValue(list[0])
+//
+//                    mMatchIdList.add(list[0].leagueId)
+//                }
+//
+////                                데이터베이스에 puuid를 꺼내옴.
+//                realtimeDB.getReference("Summoner").addValueEventListener(object :
+//                    ValueEventListener {
+//                    override fun onDataChange(snapshot: DataSnapshot) {
+//                        val value = snapshot.children.last().child(summonerInfo.puuid).key.toString()
+//
+//                        Log.d("value", value)
+////                                       puuid넣은 최근 30게임 전적이름 가져와서 데이터베이스 넣어줌.
+//
+//                    }
+//
+//                    override fun onCancelled(error: DatabaseError) {
+//
+//                    }
+//                })
+//
+//
+//            }
+//
+//            override fun onFailure(call: Call<List<LeagueResponse>>, t: Throwable) {
+//
+//            }
+//        })
 
     }
 
@@ -229,23 +98,26 @@ class SearchResultActivity : BaseActivity() {
 
 
 //        제일 위에 프사쪽 UI 매칭
-        realtimeDB.getReference("Summoner").addValueEventListener(object :ValueEventListener{
+        realtimeDB.getReference("Summoner").addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                val value = snapshot.children.last().child(summonerInfo.profileIconId.toString()).key.toString()
+                val value = snapshot.children.last()
+                    .child(summonerInfo.profileIconId.toString()).key.toString()
                 val value1 = snapshot.children.last().child(summonerInfo.name).key.toString()
-                val value2 = snapshot.children.last().child(summonerInfo.summonerLevel.toString()).key.toString()
+                val value2 = snapshot.children.last()
+                    .child(summonerInfo.summonerLevel.toString()).key.toString()
 
-                Log.d("값" , value)
+                Log.d("값", value)
                 Log.d("값", "Value1 is" + value1)
 
                 val ImageView: ImageView = findViewById(R.id.imgChamp)
-                Glide.with(mContext).load("https://ddragon.leagueoflegends.com/cdn/12.5.1/img/profileicon/${value}.png")
+                Glide.with(mContext)
+                    .load("https://ddragon.leagueoflegends.com/cdn/12.5.1/img/profileicon/${value}.png")
                     .into(ImageView)
 
-                val TitleText : TextView = findViewById(R.id.txtUserName)
+                val TitleText: TextView = findViewById(R.id.txtUserName)
                 TitleText.setText(value1)
 
-                val Level : TextView = findViewById(R.id.txtUserLevel)
+                val Level: TextView = findViewById(R.id.txtUserLevel)
                 Level.setText(value2)
 
             }
@@ -255,9 +127,91 @@ class SearchResultActivity : BaseActivity() {
             }
         })
 
+        getMyLeagueInfoList()
+
+        getMy20MatchList()
+    }
+
+    fun getMyLeagueInfoList() {
 
 
+//        json파싱만 사용해서 데이터 테이블에 넣어보자
+        apiList.getLeague(
+            summonerInfo.id,
+            "RGAPI-25102a0e-d805-443c-8449-e51b6a10f4c3"
+        ).enqueue(object : Callback<List<LeagueResponse>> {
+            override fun onResponse(
+                call: Call<List<LeagueResponse>>,
+                response: Response<List<LeagueResponse>>
+            ) {
+                val list = response.body()!!
 
+                mThisSeasonRankList.clear()
+                mThisSeasonRankList.addAll(list)
+
+                mAdapter.notifyDataSetChanged()
+            }
+
+            override fun onFailure(call: Call<List<LeagueResponse>>, t: Throwable) {
+
+            }
+        })
+
+    }
+
+    fun getMy20MatchList() {
+
+        apiList3.getMatch(
+            summonerInfo.puuid,
+            "20",
+            "RGAPI-25102a0e-d805-443c-8449-e51b6a10f4c3"
+        )
+            .enqueue(object : Callback<List<String>> {
+                override fun onResponse(
+                    call: Call<List<String>>,
+                    response: Response<List<String>>
+                ) {
+                    val matchIdList = response.body()!!
+                    Log.d("매치id목록", matchIdList.toString())
+
+                    for (matchId in matchIdList) {
+
+                        apiList3.getMatchDetail(
+                            matchId,
+                            "RGAPI-25102a0e-d805-443c-8449-e51b6a10f4c3"
+                        ).enqueue(object :
+                            Callback<MatchDetailData> {
+                            override fun onResponse(
+                                call: Call<MatchDetailData>,
+                                response: Response<MatchDetailData>
+                            ) {
+                                val matchDetail = response.body()!!
+
+                                Log.d("matchDetail", matchDetail.toString())
+
+                                mMatchDetailList.add(matchDetail)
+                                mAdapter.notifyDataSetChanged()
+                            }
+
+                            override fun onFailure(
+                                call: Call<MatchDetailData>,
+                                t: Throwable
+                            ) {
+
+                            }
+                        })
+                    }
+
+                }
+
+                override fun onFailure(
+                    call: Call<List<String>>,
+                    t: Throwable
+                ) {
+
+                }
+
+            })
 
     }
 }
--- app/src/main/java/com/zeroheat/lolandroll/adapters/SearchUserrRecyclerAdapter.kt
+++ app/src/main/java/com/zeroheat/lolandroll/adapters/SearchUserrRecyclerAdapter.kt
@@ -23,14 +23,10 @@ import retrofit2.Response
 class SearchUserrRecyclerAdapter(
     val mContext: Context,
 
+    val myPuuId: String,
     val mFirstList : ArrayList<FirstData>,
-    val mSecondList : ArrayList<SecondData>,
-    val mThirdList : ArrayList<ThirdData>,
-
-
-//    val mRankList: ArrayList<LeagueResponse>,
-//    val mLeagueList: ArrayList<LeagueResponse>,
-//    val mMatchIdList: List<String>,
+    val mThisSeasonRankList : ArrayList<LeagueResponse>,
+    val mMatchDetailList : ArrayList<MatchDetailData>,
 
 
     ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -55,123 +51,124 @@ class SearchUserrRecyclerAdapter(
 
     inner class SecondViewHolder internal constructor(itemView: View) :
         RecyclerView.ViewHolder(itemView) {
-//        var content: TextView
-//        var image : ImageView
-//
-//
-//            content = itemView.findViewById(R.id.btnRankValue)
-//            content = itemView.findViewById(R.id.txtRankNow)
-//            content = itemView.findViewById(R.id.txtLp)
-//            content = itemView.findViewById(R.id.txtWinLoss)
-//            image = itemView.findViewById(R.id.imgRank)
-//
 
-        fun bind2(data: DataItem) {
+
+        val txtRankNow = itemView.findViewById<TextView>(R.id.txtRankNow)
+
+        fun bind(rankList: List<LeagueResponse>) {
+
+            if (rankList.isNotEmpty()) {
+
+                val thisSeasonData = rankList[0]
+
+                txtRankNow.text = "${thisSeasonData.tier} ${thisSeasonData.rank}"
+            }
+
+
         }
-
-
     }
 
     inner class ThirdViewHolder internal constructor(itemView: View) :
         RecyclerView.ViewHolder(itemView) {
 
-        val txtGameMode = itemView.findViewById<TextView>(R.id.txtGameMode)
-//        val txtWin = itemView.findViewById<TextView>(R.id.txtWin)
+            val txtGameMode = itemView.findViewById<TextView>(R.id.txtGameMode)
+            val txtKda = itemView.findViewById<TextView>(R.id.txtKda)
 
 
-        fun bind3(data: String) {
+            fun bind(data: MatchDetailData) {
 
-            txtGameMode.text = data
+                txtGameMode.text = data.info.gameMode
+                Log.d("이 판 id", data.info.gameId.toString())
+
+                for (gamer in data.info.participants) {
+                    Log.d("이 판 참여자목록", gamer.summonerName)
+
+                    if (gamer.puuid == myPuuId) {
+                        Log.d("내 라인?", gamer.role)
+
+                        txtKda.text = "${gamer.kills}/${gamer.deaths}/${gamer.assists}"
+                    }
+
+                }
+            }
         }
 
-
-    }
 
     override fun getItemViewType(position: Int): Int {
 
 //            홀수줄 : 데이터목록
 //            짝수줄 : 챔프목록
-        return when (position) {
-            0 -> code.ViewType.multi_type1
-            1 -> code.ViewType.multi_type2
-            else -> code.ViewType.multi_type3
-        }
-    }
+                return when (position) {
+                    0 -> code.ViewType.multi_type1
+                    1 -> code.ViewType.multi_type2
+                    else -> code.ViewType.multi_type3
+                }
+            }
 
-    //    어떤 xml을 inflate해서 사용할건지
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val view: View
-        val context = mContext
-        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        return if (viewType == code.ViewType.multi_type1) {
-            view = inflater.inflate(R.layout.first_content, parent, false)
-            FirstViewHolder(view)
-        } else if (viewType == code.ViewType.multi_type2) {
-            view = inflater.inflate(R.layout.second_content, parent, false)
-            SecondViewHolder(view)
-        } else {
-            view = inflater.inflate(R.layout.third_content, parent, false)
-            ThirdViewHolder(view)
-        }
+            //    어떤 xml을 inflate해서 사용할건지
+            override fun onCreateViewHolder(
+                parent: ViewGroup,
+                viewType: Int
+            ): RecyclerView.ViewHolder {
+                val view: View
+                val context = mContext
+                val inflater =
+                    context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+                return if (viewType == code.ViewType.multi_type1) {
+                    view = inflater.inflate(R.layout.first_content, parent, false)
+                    FirstViewHolder(view)
+                } else if (viewType == code.ViewType.multi_type2) {
+                    view = inflater.inflate(R.layout.second_content, parent, false)
+                    SecondViewHolder(view)
+                } else {
+                    view = inflater.inflate(R.layout.third_content, parent, false)
+                    ThirdViewHolder(view)
+                }
 
-    }
+            }
 
-    //  목록이 몇개인지
-    override fun getItemCount() =
-        mFirstList.size + mSecondList.size + mThirdList.size // 앞의 두개는 매치 목록이 아님
+            //  목록이 몇개인지
+            override fun getItemCount() =
+                mMatchDetailList.size + 2 // 앞의 두개는 매치 목록이 아님
 
 
-    //    위치에 맞는 데이터 추출
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            //    위치에 맞는 데이터 추출
+            override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
 
 
 //        뷰타입이 1번일때는
-        if (holder.itemViewType == code.ViewType.multi_type1) {
+                if (holder.itemViewType == code.ViewType.multi_type1) {
 //            val data1 = mRankList[position]
 
-        }
+                }
 //        뷰타입이 2번일때는
-        if (holder.itemViewType == code.ViewType.multi_type2) {
+                if (holder.itemViewType == code.ViewType.multi_type2) {
 //            val data2 = mLeagueList[position]
 //            Log.d("data2", data2.toString())
-        }
+
+                    (holder as SecondViewHolder).bind(mThisSeasonRankList)
+                }
 //        뷰타입이 3번일때는
 
-        if (holder.itemViewType == code.ViewType.multi_type3) {
-            val data = mThirdList[position - 2] // 0,1번째는 다른거니까
-            Log.d("data값", data)
+                if (holder.itemViewType == code.ViewType.multi_type3) {
+                    val data = mMatchDetailList[position - 2] // 0,1번째는 다른거니까
 
-            (holder as ThirdViewHolder).bind3(data)
+                    (holder as ThirdViewHolder).bind(data)
 
 
-            val retrofit = AsiaServerAPI.getRetrofit(mContext)
-            val apiList3 = retrofit.create(API3List::class.java)
+                    val retrofit = AsiaServerAPI.getRetrofit(mContext)
+                    val apiList3 = retrofit.create(API3List::class.java)
 
-//            apiList3.getMatchDetail(
-//                data,
-//                "RGAPI-bb301326-0c05-4b58-a7c1-80fe997968aa").enqueue(object :
-//                Callback<MatchDetailData> {
-//                override fun onResponse(
-//                    call: Call<MatchDetailData>,
-//                    response: Response<MatchDetailData>
-//                ) {
-//                    val b = response.body()!!
-//                    Log.d("왜안되는건데", b.toString())
-//
-//                }
-//
-//                override fun onFailure(
-//                    call: Call<MatchDetailData>,
-//                    t: Throwable
-//                ) {
-//
-//                }
-//            })
 
+                }
 
         }
-
     }
 
-}
+
+
+
+
+
+
 
