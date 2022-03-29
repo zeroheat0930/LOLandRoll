a1eb2bac9c7eca353ca7140b25b9a1bad382bcfa
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-29 10:50:21 +0900

    SpellDataUtil 데이터 싹다 넣음

--- app/src/main/java/com/zeroheat/lolandroll/utils/SpellDataUtil.kt
+++ app/src/main/java/com/zeroheat/lolandroll/utils/SpellDataUtil.kt
@@ -5,8 +5,24 @@ class SpellDataUtil {
     companion object {
 
         val spellHashMap = hashMapOf(
+            1 to "SummonerBoost",
+            3 to "SummonerExhaust",
             4 to "SummonerFlash",
+            6 to "SummonerHaste",
+            7 to "SummonerHeal",
+            11 to "SummonerSmite",
+            12 to "SummonerTeleport",
+            13 to "SummonerMana",
             14 to "SummonerDot",
+            21 to "SummonerBarrier",
+            30 to "SummonerPoroRecall",
+            31 to "SummonerPoroThrow",
+            32 to "SummonerSnowball",
+            39 to "SummonerSnowURFSnowball_Mark",
+            54 to "Summoner_UltBookPlaceholder",
+            55 to "Summoner_UltBookSmitePlaceholder"
+
+
         )
 
     }
